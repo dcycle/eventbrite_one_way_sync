8a6786372715c7793c690f493be9a1fe857da424
Albert Albala <albert@mediatribe.net>
2024-10-24 15:25:48 -0400

    Support Drupal 11 (#14)

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,10 +7,10 @@ set -e
 echo '=> Run fast tests.'
 ./scripts/test.sh
 
-echo '=> Deploy a Drupal 9 environment.'
-./scripts/deploy.sh 9
+echo '=> Deploy a Drupal 11 environment.'
+./scripts/deploy.sh 11
 
-echo '=> Tests on Drupal 9 environment.'
+echo '=> Tests on Drupal 11 environment.'
 ./scripts/test-running-environment.sh
 
 echo '=> Request-response tests.'
@@ -19,7 +19,7 @@ echo '=> Request-response tests.'
 echo 'Make sure it is possible to uninstall the module'
 docker-compose exec -T drupal /bin/bash -c 'drush pmu -y eventbrite_one_way_sync'
 
-echo '=> Destroy the Drupal 9 environment.'
+echo '=> Destroy the Drupal 11 environment.'
 ./scripts/destroy.sh
 
 echo '=> Deploy a Drupal 10 environment.'
